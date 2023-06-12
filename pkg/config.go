cae11be974ae2d0de17fc6b6d39eda1f5dcf2ff6
Antonio Bello <ab3llo15@gmail.com>
2023-06-12 12:11:49 +0100

    initial commit

---
+++ pkg/config.go
@@ -0,0 +1,31 @@
+package config
+
+import "github.com/spf13/viper"
+
+type DatabaseConfig struct {
+	Port     string `mapstructure:"DB_PORT"`
+	Name     string `mapstructure:"DB_NAME"`
+	Host     string `mapstructure:"DB_HOST"`
+	User     string `mapstructure:"DB_USER"`
+	Password string `mapstructure:"DB_PASSWORD"`
+}
+
+type Config struct {
+	ServerName string `mapstructure:"SERVER_NAME"`
+	Port       string `mapstructure:"SERVER_PORT"`
+	DbConfig   DatabaseConfig
+}
+
+func LoadConfig() (cfg Config, err error) {
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&cfg)
+	return
+}
